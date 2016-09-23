6f9d581983793d084b39e4f522fc85c380c3f351
James Robb <me@jamesrobb.ca>
2016-09-23 19:31:00 +0000

    making file signature pretty

--- src/log.h
+++ src/log.h
@@ -9,9 +9,9 @@
 const gchar * log_level_to_string (GLogLevelFlags level);
 
 void httpd_log_all_handler_cb (const gchar *log_domain, 
-					 GLogLevelFlags log_level, 
-					 const gchar *message,
-					 gpointer user_data);
+							   GLogLevelFlags log_level, 
+							   const gchar *message,
+							   gpointer user_data);
 
 
 #endif
