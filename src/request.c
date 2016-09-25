d99df8c307ab53e54a01cf2f30f781dd9cb2b6ef
James Robb <me@jamesrobb.ca>
2016-09-25 01:37:11 +0000

    gathering other field/values from http request

--- src/request.c
+++ src/request.c
@@ -1,21 +1,24 @@
 #include "request.h"
 
 int parse_http_request(char* data_buffer, http_request *request) {
-	gchar *delimiters_line = "\n";
-	gchar **buffer_split_line = g_strsplit((gchar *) data_buffer, delimiters_line, 0);
 
-	request->value_table = g_hash_table_new(g_str_hash, g_str_equal);
+	bool return_with_error = FALSE;
+	gchar **buffer_split_line = g_strsplit((gchar *) data_buffer, NEWLINE_DELIM, 0);
+
+	request->value_table = g_hash_table_new_full(g_str_hash, g_str_equal, ghash_table_gchar_destroy, ghash_table_gchar_destroy);
 
 	// yay pointer arithematic
 	for(int i = 0; buffer_split_line[i]; i++) {
 
 		gchar **current_line;
+		return_with_error = FALSE;
 
-		// special case for first line of HTTP requests (method, uri, http version)
+		// special case for first line of HTTP request, the request line (method, uri, http version)
 		if(i == 0) {
 
-			current_line = g_strsplit(buffer_split_line[i], " ", 0);
+			current_line = g_strsplit(buffer_split_line[i], REQUEST_LINE_DELIM, 0);
 
+			int token_count = 0;
 			for(int j = 0; current_line[j]; j++) {
 
 				bool val_set = FALSE;
@@ -25,28 +28,75 @@ int parse_http_request(char* data_buffer, http_request *request) {
 				// we get the http method, uri, and version
 				if(j == 0) {
 					val_set = TRUE;
-					g_hash_table_insert(request->value_table, "http_method", value);
+					gchar *key = g_malloc(12);
+					g_stpcpy(key, "http_method\0");
+					g_hash_table_insert(request->value_table, key, value);
 				}
 				if(j == 1) {
 					val_set = TRUE;
-					g_hash_table_insert(request->value_table, "http_uri", value);
+					gchar *key = g_malloc(9);
+					g_stpcpy(key, "http_uri\0");
+					g_hash_table_insert(request->value_table, key, value);
 				}
 				if(j == 2) {
 					val_set = TRUE;
-					g_hash_table_insert(request->value_table, "http_version", value);
+					gchar *key = g_malloc(13);
+					g_stpcpy(key, "http_version\0");
+					g_hash_table_insert(request->value_table, key, value);
 				}
 
 				if(val_set == FALSE) {
 					g_free(value);
+				} else {
+					token_count++;
 				}
+			}
+
+			if(token_count != 3) {
+				g_warning("malformed http request (method, uri, version)");
+				return_with_error = TRUE;
+			}
+
+		} else {
+
+			if(g_strcmp0(buffer_split_line[i], "") == 0) {
+				// end of header fields (hopefully)
+				break;
+			}
+
+			current_line = g_strsplit(buffer_split_line[i], REQUEST_FIELD_DELIM, 0);
+
+			if(current_line[0] && current_line[1]) {
+
+				gchar *key = g_malloc(gchar_array_len(current_line[0]));
+				g_stpcpy(key, current_line[0]);
+
+				gchar *value = g_malloc(gchar_array_len(current_line[1]));
+				g_stpcpy(value, current_line[1]);
+
+				g_hash_table_insert(request->value_table, key, value);
+
+			} else {
+				g_warning("malformed http request (fields)");
+				return_with_error = TRUE;
 			}
 
 		}
 
 		g_strfreev(current_line);
-		break;
+		
+		if(return_with_error == TRUE) {
+			break;
+		}
+
 	}
 
 	g_strfreev(buffer_split_line);
-	return 0;
+
+	return (return_with_error == TRUE ? 1 : 0);
+}
+
+void http_request_print(http_request *request) {
+	g_hash_table_foreach(request->value_table, (GHFunc)ghash_table_strstr_iterator, "field: %s, value: %s\n");
+	return;
 }
--- src/request.h
+++ src/request.h
@@ -7,11 +7,17 @@
 #include "log.h"
 #include "util.h"
 
+#define NEWLINE_DELIM		((gchar *) "\r\n")
+#define REQUEST_LINE_DELIM	((gchar *) " ")
+#define REQUEST_FIELD_DELIM	((gchar *) ": ")
+
 typedef struct _http_request
 {
 	GHashTable *value_table;
 } http_request;
 
 int parse_http_request(char* data_buffer, http_request *request);
+
+void http_request_print(http_request *request);
 
 #endif
