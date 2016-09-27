ccde6a5d5b1f8a329a3a2d33a4de4bf04b14f60b
Sigurdur Helgason <sigurdurhel15@ru.is>
2016-09-27 16:27:06 +0000

    added a function that reads http query part to hashmap

--- src/request.c
+++ src/request.c
@@ -100,3 +100,28 @@ void http_request_print(http_request *request) {
 	g_hash_table_foreach(request->value_table, (GHFunc)ghash_table_strstr_iterator, "field: %s, value: %s\n");
 	return;
 }
+
+int http_request_parse_queries(gchar *http_uri, GHashTable *key_value_table) {
+
+	// first try to get anything in a query field 
+	// query field is anything after ? in the http uri
+	gchar **initial_split = g_strsplit(http_uri, REQUEST_URI_DELIM, 0);
+
+	// if we have multiple question marks return error
+	if(initial_split[2]) {
+		return -1;
+	}
+
+	// if we managed to split at ? we get queries at the back in field 2
+	if(initial_split[1]) {
+		// queries are like this key=value&key=value and so on
+		gchar **split_queries = g_strsplit(initial_split[1], REQUEST_QUERY_DELIM, 0);
+		int current_index = 0;
+		while(split_queries[current_index]){
+			gchar **split_key_values = g_strsplit(split_queries[current_index], REQUEST_QUERY_KEY_VALUE_DELIM, 2);
+			g_hash_table_insert(key_value_table, split_key_values[0], split_key_values[1]);
+			current_index++;
+		}
+	}
+	return 0;
+}
