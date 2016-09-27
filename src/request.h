4f85b6faea75ec842b73d4afd34742db5e248900
Sigurdur Helgason <sigurdurhel15@ru.is>
2016-09-27 16:27:23 +0000

    added definitions for delimiters and function

--- src/request.h
+++ src/request.h
@@ -10,6 +10,9 @@
 #define NEWLINE_DELIM		((gchar *) "\r\n")
 #define REQUEST_LINE_DELIM	((gchar *) " ")
 #define REQUEST_FIELD_DELIM	((gchar *) ": ")
+#define REQUEST_URI_DELIM ((gchar *) "?")
+#define REQUEST_QUERY_DELIM ((gchar *) "&")
+#define REQUEST_QUERY_KEY_VALUE_DELIM ((gchar *) "=")
 
 typedef struct _http_request
 {
@@ -19,5 +22,7 @@ typedef struct _http_request
 int parse_http_request(char* data_buffer, http_request *request);
 
 void http_request_print(http_request *request);
+
+int http_request_parse_queries(gchar *http_uri, GHashTable *key_value_table);
 
 #endif
