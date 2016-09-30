607e033506d6ac3cb69906d4f1d6cdfe67bd5c40
James Robb <me@jamesrobb.ca>
2016-09-30 16:34:14 +0000

    making sure the constants dont get redefined

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,10 @@
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+
 #define MAX_CLIENT_CONNS    50
 #define MAX_CONN_BACKLOG    10
 #define DATA_BUFFER_LENGTH  77056 // 64KB
 #define CONN_FREE           -1
 #define NAOUZ_VERSION       ((gchar*) "0.2")
+
+#endif
